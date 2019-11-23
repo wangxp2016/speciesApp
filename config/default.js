a5b136673de1ce0a767352a1c6e676af1edb0382
wangxp2016 <wangxp_2014@163.com>
2019-11-23 13:19:38 +0800

    修改数据库配置

--- config/default.js
+++ config/default.js
@@ -9,7 +9,7 @@ const config = {
     db: {
         database: 'juyi',
         user: 'root',
-        password: '123456',
+        password: 'hao13205340362',
         host: '127.0.0.1',
         port: '3306'
     }
